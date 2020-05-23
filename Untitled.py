7afe4fef49510e7a3e75417799a56d734d40fd1f
mukul-shaunik <shaunikmail@gmail.com>
2020-05-24 00:59:45 +0530

    added model file

--- Untitled.py
+++ Untitled.py
@@ -1,59 +1,70 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[ ]:
 
 
 from keras.datasets import mnist
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Flatten
+import sys
+
+first_filter_size = int(sys.argv[1])
+count_conv_layer = int(sys.argv[2])
+count_epoch = int(sys.argv[3])
 
 
-# In[2]:
+# In[ ]:
 
 
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
 
-# In[5]:
+# In[ ]:
 
 
 X_train = X_train.reshape(60000,28,28,1)
 X_test = X_test.reshape(10000,28,28,1)
 
 
-# In[6]:
+# In[ ]:
 
 
-from keras.utils import to_categorical#one-hot encode target column
 y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
 
 
-# In[32]:
+# In[ ]:
 
 
-from keras.models import Sequential
-from keras.layers import Dense, Conv2D, Flatten#create model
 model = Sequential()#add model layers
-model.add(Conv2D(1, kernel_size=1, activation='relu', input_shape=(28,28,1)))
+model.add(Conv2D(first_filter_size, kernel_size=1, activation='relu', input_shape=(28,28,1)))
+for i in range(count_conv_layer):
+	model.add(Conv2D(first_filter_size//(2**i), kernel_size=1, activation='relu'))
 model.add(Flatten())
 model.add(Dense(10, activation='softmax'))
 
 
-# In[33]:
+# In[ ]:
 
 
-#compile model using accuracy to measure model performance
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 
-# In[34]:
+# In[ ]:
 
 
-#train the model
-history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=1)
+history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=count_epoch)
 
 
-# In[19]:
+# In[ ]:
+
+
+history.history['accuracy']
+
+
+# In[ ]:
 
 
 history.history['accuracy']
